7fa3aa120a0a758e4af202884f61caea04e7c4e5
Aleksei Obukhov <alexey.obuhov@gmail.com>
2023-09-25 13:53:25 +0300

    docker compose files

---
+++ docker-product-server/src/test/java/com/example/dockerproductserver/DockerProductServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerproductserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerProductServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
