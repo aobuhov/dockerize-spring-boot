8f6d2ed23b4852f02fda884f0f40a4a2e6f09e88
Aleksei Obukhov <alexey.obuhov@gmail.com>
2023-09-25 13:52:20 +0300

    simple message server

---
+++ docker-message-server/src/test/java/com/example/dockermessageserver/DockerMessageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockermessageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerMessageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
